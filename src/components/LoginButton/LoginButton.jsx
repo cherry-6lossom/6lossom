36e3299c9536b8120a50162adf4670c857cc2751
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-03-14 20:13:23 +0900

    [feat] 페이스북을 통해 로그인한 회원을 파이어베이스 인증 사용자에 등록하는 기능, 회원의 uid 정보를 세션스토리지에 추가하는 기능 구현 #1

---
+++ src/components/LoginButton/LoginButton.jsx
@@ -0,0 +1,27 @@
+import { useCreateAuthUser } from '@/firebase/auth/useCreateAuthUser';
+import { signInWithPopup } from 'firebase/auth';
+import { useEffect } from 'react';
+import { auth } from '@/firebase/app';
+
+const LoginButton = ({ provider, text, setUid }) => {
+  const { createAuthUser, isLoading, error } = useCreateAuthUser('authUsers');
+
+  const handleLoginClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      setUid(data.user.email);
+      sessionStorage.setItem('uid', data.user.uid);
+      createAuthUser(data.user);
+    });
+  };
+
+  useEffect(() => {
+    setUid(sessionStorage.getItem('uid'));
+  });
+  return (
+    <>
+      <button onClick={handleLoginClick}>Sigin with {text}</button>
+    </>
+  );
+};
+
+export default LoginButton;
--- src/firebase/app.js
+++ src/firebase/app.js
@@ -1,4 +1,11 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  TwitterAuthProvider,
+} from 'firebase/auth';
 
 const {
   VITE_API_KEY,
@@ -18,5 +25,10 @@ const firebaseConfig = {
   appId: VITE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
-
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const twitterProvider = new TwitterAuthProvider();
+export { auth, googleProvider, facebookProvider, twitterProvider };
