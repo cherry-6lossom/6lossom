7d3f5f1e6104056af67f3fa5b3c7a3b6dd597606
SSY1203 <tlstjsdud566@gmail.com>
2023-03-15 17:04:23 +0900

    [refactor] 사용하지 않는 코드 제거 #65

--- src/pages/MakeTreePage/MakeTreePage.jsx
+++ src/pages/MakeTreePage/MakeTreePage.jsx
@@ -1,15 +1,12 @@
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useReadData } from '@/firebase/firestore/useReadData';
 import style from './MakeTreePage.module.scss';
 import HeaderTitle from '@/components/HeaderTitle/HeaderTitle';
 import UsageDescription from '@/components/UsageDescription/UsageDescription';
 
 import blossomTree from '@/assets/main-page/main-tree.png';
 import BackgroundCustomList from '@/components/BackgroundCustomList/BackgroundCustomList';
-import ShortButton from './../../components/ShortButton/ShortButton';
 import ShortButtonList from './../../components/ShortButtonList/ShortButtonList';
-
+import classNames from 'classnames';
 const MakeTreePage = () => {
   const navigate = useNavigate();
 
@@ -21,28 +18,26 @@ const MakeTreePage = () => {
   const displayName = JSON.parse(localStorage.getItem('user'));
 
   return (
-    <>
-      <div className={style.makeTreeContainer}>
-        <header className={style.makeTreeHeader}>
-          <UsageDescription subText={'벚꽃나무에 이름을 적어주세요'} />
-          <HeaderTitle
-            mainText={`${displayName ? displayName + '님의 벚꽃나무' : ''}`}
-          />
-        </header>
-        <button onClick={logout}>Logout</button>
-        <div className={style.blossomTree}>
-          <img src={blossomTree} alt="벚꽃나무" />
-        </div>
-        <div className={style.makeTreeCustom}>
-          <UsageDescription
-            makeTreeCustomText={'makeTreeCustomText'}
-            subText={'벚꽃나무의 하늘을 선택해주세요.'}
-          />
-          <BackgroundCustomList />
-          <ShortButtonList firstText={'취소'} secondText={'완료'} />
-        </div>
+    <div className={classNames('MakeTreePage', style.makeTreeContainer)}>
+      <header className={style.makeTreeHeader}>
+        <UsageDescription subText={'벚꽃나무에 이름을 적어주세요'} />
+        <HeaderTitle
+          mainText={`${displayName ? displayName + '님의 벚꽃나무' : ''}`}
+        />
+      </header>
+      <button onClick={logout}>Logout</button>
+      <div className={style.blossomTree}>
+        <img src={blossomTree} alt="벚꽃나무" />
       </div>
-    </>
+      <div className={style.makeTreeCustom}>
+        <UsageDescription
+          makeTreeCustomText={'makeTreeCustomText'}
+          subText={'벚꽃나무의 하늘을 선택해주세요.'}
+        />
+        <BackgroundCustomList />
+        <ShortButtonList firstText={'취소'} secondText={'완료'} />
+      </div>
+    </div>
   );
 };
 
