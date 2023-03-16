16f0d9a1fcbb4abbc872116a84c46dd56d8dc377
SSY1203 <tlstjsdud566@gmail.com>
2023-03-16 17:10:23 +0900

    [refactor] 클래스명 가독성 있게 수정 #70

--- src/components/UsageDescription/UsageDescription.jsx
+++ src/components/UsageDescription/UsageDescription.jsx
@@ -4,9 +4,7 @@ import style from './UsageDescription.module.scss';
 const UsageDescription = ({ className = '', subText }) => {
   return (
     <>
-      <h4 className={classNames(`${className}`, style.makeTreeSubTitle)}>
-        {subText}
-      </h4>
+      <h4 className={classNames(`${className}`, style.subTitle)}>{subText}</h4>
     </>
   );
 };
