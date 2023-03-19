b8f937a33b97b1a2d5eedc5b1836f8324c3290bf
Eonii2 <eoniioff@gmailcom>
2023-03-20 02:53:55 +0900

    [feat] author 과 content 내용 입력할 수 있도록함 #25

--- src/components/MessageInputContainer/MessageInputContainer.jsx
+++ src/components/MessageInputContainer/MessageInputContainer.jsx
@@ -2,33 +2,55 @@ import classes from '@/components/MessageInputContainer/MessageInputContainer.mo
 import LongButton from '../LongButton/LongButton';
 import UsageDescription from '../UsageDescription/UsageDescription';
 import HeaderTitle from '../HeaderTitle/HeaderTitle';
-
+import { useState } from 'react';
 const MessageInputContainer = () => {
+  const [state, setState] = useState({
+    author: '',
+    content: '',
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert('한번남긴 편지는 삭제할 수 없어요');
+  };
+
   return (
     <>
       <div className={classes.messageWrap}>
         <HeaderTitle mainText={'육캔두잇님의 벚꽃나무'} />
-        {/* <h1 className={classes.title}>육캔두잇님의 벚꽃나무</h1> */}
         <HeaderTitle />
         <UsageDescription
           makeTreeCustomText={'makeTreeCustomText'}
           subText={'메세지를 남겨주세요'}
         />
-        <p>텍스트 입력</p>
-        <div className={classes.inputContainer}>
+        <div className={classes.authorContainer}>
           <input
             className={classes.nickname}
+            name="author"
             placeholder="작성자이름을 적어주세요"
             type="text"
-          />
-          <input
-            className={classes.message}
-            placeholder="메세지를 작성해주세요"
-            type="text"
+            value={state.author}
+            onChange={handleChangeState}
           />
         </div>
-        <LongButton />
-        <LongButton />
+        <div className={classes.contentContainer}>
+          <textarea
+            className={classes.message}
+            name="content"
+            placeholder="메세지를 작성해주세요"
+            type="text"
+            value={state.content}
+            onChange={handleChangeState}
+          />
+        </div>
+        <button onClick={handleSubmit}>완료</button>
       </div>
     </>
   );
