59ea766134bf010ab2cad346bae045dd3f85e286
Eonii2 <eoniioff@gmailcom>
2023-03-20 01:23:25 +0900

    [style] 메세지창 스타일 적용 #25

--- src/components/MessageInputContainer/MessageInputContainer.jsx
+++ src/components/MessageInputContainer/MessageInputContainer.jsx
@@ -1,25 +1,35 @@
-import classes from '@/components/ModalEnroll/ModalEnroll.module.scss';
+import classes from '@/components/MessageInputContainer/MessageInputContainer.module.scss';
 import LongButton from '../LongButton/LongButton';
+import UsageDescription from '../UsageDescription/UsageDescription';
+import HeaderTitle from '../HeaderTitle/HeaderTitle';
 
 const MessageInputContainer = () => {
   return (
     <>
-      <h1 className={classes.title}>육캔두잇님의 벚꽃나무</h1>
-      <p>텍스트 입력</p>
-      <div className={classes.inputContainer}>
-        <input
-          className={classes.nickname}
-          placeholder="작성자이름을 적어주세요"
-          type="text"
+      <div className={classes.messageWrap}>
+        <HeaderTitle mainText={'육캔두잇님의 벚꽃나무'} />
+        {/* <h1 className={classes.title}>육캔두잇님의 벚꽃나무</h1> */}
+        <HeaderTitle />
+        <UsageDescription
+          makeTreeCustomText={'makeTreeCustomText'}
+          subText={'메세지를 남겨주세요'}
         />
-        <input
-          className={classes.message}
-          placeholder="메세지를 작성해주세요"
-          type="text"
-        />
+        <p>텍스트 입력</p>
+        <div className={classes.inputContainer}>
+          <input
+            className={classes.nickname}
+            placeholder="작성자이름을 적어주세요"
+            type="text"
+          />
+          <input
+            className={classes.message}
+            placeholder="메세지를 작성해주세요"
+            type="text"
+          />
+        </div>
+        <LongButton />
+        <LongButton />
       </div>
-      <LongButton />
-      <LongButton />
     </>
   );
 };
