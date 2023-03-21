b76e78cb863b57700bd4f605905bae02a215a2d4
Eonii2 <eoniioff@gmailcom>
2023-03-21 22:40:30 +0900

    [style] 컴포넌트 배열 맞추기 #25

---
+++ src/pages/WriteMessagePage/WriteMessagePage.jsx
@@ -0,0 +1,36 @@
+import classes from '@/pages/WriteMessagePage/WriteMessagePage.module.scss';
+import classNames from 'classnames';
+import style from '@/pages/WriteMessagePage/WriteMessagePage.module.scss';
+import HeaderTitle from '@/components/HeaderTitle/HeaderTitle';
+import LongButton from '@/components/LongButton/LongButton';
+import MessageInputContainer from '@/components/MessageInputContainer/MessageInputContainer';
+import UsageDescription from '@/components/UsageDescription/UsageDescription';
+import LongButtonList from './../../components/LongButtonList/LongButtonList';
+
+const WriteMessagePage = () => {
+  const handlePrev = () => {};
+  const handleMakeMsg = () => {};
+
+  return (
+    <>
+      <div className={classes.writeMessageWrap}>
+        <header className={classes.writeMessageHeader}>
+          <HeaderTitle mainText={'육캔두잇님의 벚꽃나무'} />
+          <UsageDescription
+            makeTreeCustomText={'makeTreeCustomText'}
+            subText={'메세지를 남겨주세요'}
+          />
+        </header>
+        <MessageInputContainer />
+        <LongButtonList
+          firstText={'이전'}
+          firstClick={handlePrev}
+          secondText={'완료'}
+          secondClick={handleMakeMsg}
+        />
+      </div>
+    </>
+  );
+};
+
+export default WriteMessagePage;
