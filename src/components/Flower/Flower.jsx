b5fa0f4361fff45daa1523a6e4d2afabafad46fe
SSY1203 <tlstjsdud566@gmail.com>
2023-03-22 19:31:41 +0900

    [feature] 벚꽃 클릭 이벤트, 전체 메세지 버튼 이벤트 #11 #12 #34 #35

---
+++ src/components/Flower/Flower.jsx
@@ -0,0 +1,41 @@
+import style from './Flower.module.scss';
+import classNames from 'classnames';
+import messageContext from '@/contexts/messageContext';
+import { useContext } from 'react';
+
+const Flower = ({ item, handleOpenMessageDetail }) => {
+  const messageVisibility = useContext(messageContext);
+  const { messageDetailVisible, setMessageDetailVisible } = messageVisibility;
+  const { id, flowerSrc, nickname, contents } = item;
+
+  return (
+    <>
+      <li
+        className={classNames(
+          style.flower,
+          id % 6 === 0
+            ? style.flower0
+            : id % 6 === 1
+            ? style.flower1
+            : id % 6 === 2
+            ? style.flower2
+            : id % 6 === 3
+            ? style.flower3
+            : id % 6 === 4
+            ? style.flower4
+            : style.flower5
+        )}
+        key={id}
+      >
+        <button
+          onClick={() => handleOpenMessageDetail(messageVisibility, item)}
+        >
+          <img src={flowerSrc} alt="벚꽃 메세지" />
+          {nickname}
+        </button>
+      </li>
+    </>
+  );
+};
+
+export default Flower;
