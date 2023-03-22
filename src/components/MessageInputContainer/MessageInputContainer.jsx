8edbf4231ff8e931da6e8f06eaf98c189258efa7
Eonii2 <eoniioff@gmailcom>
2023-03-23 03:36:41 +0900

    [style] ModalEnroll 스타일 적용 완료 #26

--- src/components/MessageInputContainer/MessageInputContainer.jsx
+++ src/components/MessageInputContainer/MessageInputContainer.jsx
@@ -1,5 +1,6 @@
 import classes from '@/components/MessageInputContainer/MessageInputContainer.module.scss';
 import { useState, useRef } from 'react';
+import ModalEnroll from '@/components/ModalEnroll/ModalEnroll';
 import LongButtonList from './../LongButtonList/LongButtonList';
 
 const MessageInputContainer = () => {
@@ -23,6 +24,7 @@ const MessageInputContainer = () => {
       setText(inputValue);
     }
   };
+  const [showModal, setShowModal] = useState(false);
 
   const handleSubmit = () => {
     if (state.author.length < 1) {
@@ -34,48 +36,55 @@ const MessageInputContainer = () => {
       return;
     }
 
-    alert('저장성공');
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
   };
 
   return (
-    <div className={classes.messageInputContainer}>
-      <div className={classes.messageWrap}>
-        <div className={classes.authorContainer}>
-          <input
-            className={classes.author}
-            name="author"
-            placeholder="작성자이름을 적어주세요"
-            type="text"
-            value={state.author}
-            onChange={handleChangeState}
-            ref={authorInput}
-            maxLength={15}
-          />
+    <>
+      <ModalEnroll show={showModal} onClose={handleCloseModal} />
+      <div className={classes.messageInputContainer}>
+        <div className={classes.messageWrap}>
+          <div className={classes.authorContainer}>
+            <input
+              className={classes.author}
+              name="author"
+              placeholder="작성자이름을 적어주세요"
+              type="text"
+              value={state.author}
+              onChange={handleChangeState}
+              ref={authorInput}
+              maxLength={15}
+            />
+          </div>
+          <div className={classes.contentContainer}>
+            <textarea
+              className={classes.content}
+              name="content"
+              placeholder="메세지를 작성해주세요"
+              type="text"
+              value={state.content}
+              onChange={(e) => {
+                handleChangeState(e);
+                handleChangeText(e);
+              }}
+              ref={contentInput}
+              maxLength={500}
+            />
+          </div>
+          <p className={classes.textLength}>{text.length} / 500</p>
         </div>
-        <div className={classes.contentContainer}>
-          <textarea
-            className={classes.content}
-            name="content"
-            placeholder="메세지를 작성해주세요"
-            type="text"
-            value={state.content}
-            onChange={(e) => {
-              handleChangeState(e);
-              handleChangeText(e);
-            }}
-            ref={contentInput}
-            maxLength={500}
-          />
-        </div>
-        <p className={classes.textLength}>{text.length} / 500</p>
+        <LongButtonList
+          firstText={'완료'}
+          firstClick={handleSubmit}
+          secondText={'이전'}
+          secondClick={() => alert('버튼 테스트 ')}
+        />
       </div>
-      <LongButtonList
-        firstText={'완료'}
-        firstClick={handleSubmit}
-        secondText={'이전'}
-        secondClick={() => alert('버튼 테스트 ')}
-      />
-    </div>
+    </>
   );
 };
 
--- src/components/ModalEnroll/ModalEnroll.jsx
+++ src/components/ModalEnroll/ModalEnroll.jsx
@@ -5,20 +5,20 @@ import ModalButtonList from '@/components/ModalButtonList/ModalButtonList';
 const ModalEnroll = () => {
   return (
     <>
-      <div className={styles.enrollWrap}>
-        <div className={styles.notice}>
-          <h5>편지를 남길까요?</h5>
+      <div className={styles.modalBackground}>
+        <div className={styles.enrollWrap}>
+          <div className={styles.notice}>
+            <h5>편지를 남길까요?</h5>
+          </div>
+          <div className={styles.noticeDetail}>
+            <p>한 번 남긴 편지는 삭제 할 수 없어요!</p>
+            <p>신중한 결정 부탁드립니다!</p>
+          </div>
+          <div className={styles.buttonList}>
+            <button className={styles.cancel}>취소</button>
+            <button className={styles.done}>완료</button>
+          </div>
         </div>
-        <div className={styles.noticeDetail}>
-          <p>한 번 남긴 편지는 삭제 할 수 없어요!</p>
-          <p>신중한 결정 부탁드립니다!</p>
-        </div>
-        <ModalButtonList
-          firstText={'취소'}
-          firstClick={() => alert('버튼클릭')}
-          secondText={'완료'}
-          secondClick={() => alert('버튼클릭')}
-        />
       </div>
     </>
   );
