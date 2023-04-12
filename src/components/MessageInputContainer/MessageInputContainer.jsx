13681bd02bd518da055d57270d8d97ce8784059e
SSY1203 <tlstjsdud566@naver.com>
2023-04-12 13:56:27 +0900

    [modify] 메세지 작성 페이지에서 input에 placeholder와 aria-label 텍스트 다르게 작성(aria-label이 더 간결하게) #214

--- src/components/MessageInputContainer/MessageInputContainer.jsx
+++ src/components/MessageInputContainer/MessageInputContainer.jsx
@@ -1,9 +1,6 @@
 import style from './MessageInputContainer.module.scss';
 
-import { useState, useRef } from 'react';
-
-import ModalEnroll from '@/components/ModalEnroll/ModalEnroll';
-import LongButtonList from '@/components/LongButtonList/LongButtonList';
+import { A11yHidden } from '../A11yHidden/A11yHidden';
 
 const MessageInputContainer = ({
   authorInput,
@@ -12,16 +9,18 @@ const MessageInputContainer = ({
   text,
   handleChangeState,
 }) => {
-  const [showModal, setShowModal] = useState(false);
 
   return (
     <div className={style.messageInputContainer}>
       <div className={style.messageWrap}>
         <div className={style.authorContainer}>
+          <A11yHidden as='label' htmlFor='nickname'>닉네임</A11yHidden>
           <input
+            id="nickname"
             className={style.author}
             name="author"
-            placeholder="작성자이름을 적어주세요"
+            aria-label='작성자 이름 작성하기 '
+            placeholder="작성자 이름을 적어주세요"
             type="text"
             value={state.author}
             onChange={handleChangeState}
@@ -30,8 +29,10 @@ const MessageInputContainer = ({
           />
         </div>
         <div className={style.contentContainer}>
+          <A11yHidden as='label' htmlFor='contents'>메세지 내용</A11yHidden>
           <textarea
-            aria-label="메세지를 작성해주세요"
+            id='contents'
+            aria-label="메세지 작성하기"
             className={style.content}
             name="content"
             placeholder="메세지를 작성해주세요"
