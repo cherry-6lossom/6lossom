3b38aad42dcac5370937af3dbc68ac9297235791
qpsqps123 <55061153+qpsqps123@users.noreply.github.com>
2023-04-17 15:09:18 +0900

    [modify] ProjectInfo의 aria-label 내용 개선 #212

--- src/components/ProjectInfoButton/ProjectInfoButton.jsx
+++ src/components/ProjectInfoButton/ProjectInfoButton.jsx
@@ -7,7 +7,7 @@ const ProjectInfoButton = ({ handleModal }) => {
         onClick={handleModal}
         className={style.clickMe}
         type="button"
-        aria-label="프로젝트 설명서"
+        aria-label="프로젝트 이용 방법"
       >
         <span className={style.clickMeText}>click me!</span>
       </button>
