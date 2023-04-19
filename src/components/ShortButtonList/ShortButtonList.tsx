c578f0570816f922038145d026ad0e3b1d5f97b7
SSY1203 <tlstjsdud566@naver.com>
2023-04-19 14:38:55 +0900

    [modify] ShortButtonList 파일 타입스크립트로 변경

--- src/components/ShortButtonList/ShortButtonList.tsx
+++ src/components/ShortButtonList/ShortButtonList.tsx
@@ -1,6 +1,16 @@
 import style from './ShortButtonList.module.scss';
 
+import React from 'react';
+
 import ShortButton from '@/components/ShortButton/ShortButton';
+
+interface ShortButtonListProp {
+  firstText:string;
+  secondText:string;
+  firstClick:()=> void;
+  secondClick?:()=>void;
+  type?:'button'|'submit';
+}
 
 const ShortButtonList = ({
   firstText,
@@ -8,12 +18,12 @@ const ShortButtonList = ({
   secondText,
   secondClick,
   type,
-}) => {
+}:ShortButtonListProp) => {
   return (
     <div className={style.ShortButtonList}>
       <ShortButton onClick={firstClick} text={firstText} />
       <ShortButton
-        onClick={secondClick ? secondClick : null}
+        onClick={secondClick}
         text={secondText}
         type={type}
       />
