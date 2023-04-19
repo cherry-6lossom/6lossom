a485cf825405de0289a5d4dd8bbbe257f5762564
SSY1203 <tlstjsdud566@naver.com>
2023-04-19 10:32:02 +0900

    [modify] UsageDescription 파일 타입스크립트로 변경

--- src/components/UsageDescription/UsageDescription.tsx
+++ src/components/UsageDescription/UsageDescription.tsx
@@ -1,10 +1,17 @@
 import style from './UsageDescription.module.scss';
 
+import React from 'react';
+
 import classNames from 'classnames';
 
 import messageIcon from '@/assets/icon/message.png';
 
-const UsageDescription = ({ className = '', subText }) => {
+interface UsageDescriptionProp {
+  className:string;
+  subText:string;
+}
+
+const UsageDescription = ({ className = '', subText }:UsageDescriptionProp) => {
   return (
     <div className={classNames(style.subTitle, className)}>
       {subText.includes('벚꽃나무') ? null : <img src={messageIcon} alt="" />}
