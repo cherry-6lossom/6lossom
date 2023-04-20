4c731b602046701b92b65bb4a53df69c1d0ae98f
SSY1203 <tlstjsdud566@gmail.com>
2023-04-20 13:18:30 +0900

    [modify] Header 파일 타입스크립트로 변경

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import style from './Header.module.scss';
 
+import React from 'react';
+
 import HeaderTitle from '@/components/HeaderTitle/HeaderTitle';
 import UsageDescription from '@/components/UsageDescription/UsageDescription';
 
-const Header = ({ userName, className, subText }) => {
+interface HeaderProp {
+  userName: string;
+  className: string;
+  subText: string;
+}
+
+const Header = ({ userName, className, subText }: HeaderProp) => {
   return (
     <header className={style.header}>
       <HeaderTitle userName={userName} />
