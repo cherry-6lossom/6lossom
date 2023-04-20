d8b71849733da23b09cad3f5bc5f091f808007a9
SSY1203 <tlstjsdud566@gmail.com>
2023-04-20 17:40:43 +0900

    [modify] ModalButtonList 파일 타입스크립트로 변경

--- src/components/ModalButtonList/ModalButtonList.tsx
+++ src/components/ModalButtonList/ModalButtonList.tsx
@@ -1,13 +1,22 @@
 import style from './WriteMessagePage.module.scss';
 
+import React from 'react';
+
 import ModalButton from '@/components/ModalButton/ModalButton';
+
+interface ModalButtonListProp {
+  firstText: string;
+  secondText: string;
+  firstClick: () => void;
+  secondClick: () => void;
+}
 
 const ModalButtonList = ({
   firstText,
   secondText,
   firstClick,
   secondClick,
-}) => {
+}: ModalButtonListProp) => {
   return (
     <>
       <div className={style.modalButtonContainer}>
