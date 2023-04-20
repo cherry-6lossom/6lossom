8ee131973e9ed8b14f9956823c9367e61f6cc66c
SSY1203 <tlstjsdud566@gmail.com>
2023-04-20 14:45:11 +0900

    [modify] Notification 파일 타입스크립트로 변경

--- src/components/Notification/Notification.tsx
+++ src/components/Notification/Notification.tsx
@@ -1,10 +1,20 @@
+import React from 'react';
+
+interface NotificationProp {
+  className: string;
+  text: string;
+  notificationRef?: React.RefObject<HTMLSpanElement>;
+  notificationRole?: string;
+  notificationAriaLive?: 'off' | 'assertive' | 'polite' | undefined;
+}
+
 const Notification = ({
   className,
   text,
   notificationRef,
   notificationRole,
   notificationAriaLive,
-}) => {
+}: NotificationProp) => {
   return (
     <span
       role={notificationRole}
